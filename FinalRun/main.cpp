f90cb694ab9f311c91855cfbf5c0e4ef8c99d547
SH-Arrow <shurt91112@gmail.com>
2016-03-24 11:40:39 -0400

    Initial Commit

---
+++ FinalRun/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FinalRun
+//
+//  Created by Steven Hurtado on 3/24/16.
+//  Copyright © 2016 GroupCOP3503. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
