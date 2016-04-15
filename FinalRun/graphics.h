e238d5d9866c48079860a07f7d97d065da2ce747
SH-Arrow <shurt91112@gmail.com>
2016-04-15 18:11:56 -0400

    Forgot resources on prior commit.

---
+++ FinalRun/graphics.h
@@ -0,0 +1,46 @@
+//
+//  graphics.h
+//  FinalRun
+//
+//  Created by Steven Hurtado on 3/24/16.
+//  Copyright © 2016 GroupCOP3503. All rights reserved.
+//
+
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+#include <map>
+#include <string>
+#include <SDL2/SDL.h>
+
+struct SDL_Window;
+struct SDL_Renderer;
+
+class Graphics
+{
+  
+private:
+    SDL_Window* _window;
+    SDL_Renderer* _renderer;
+    
+    std::map<std::string, SDL_Surface*> _spriteSheets;
+    
+public:
+    Graphics();
+    ~Graphics();
+    
+    //loads image into _spritesheetmap if it doesn't already exist
+    SDL_Surface * loadImage(const std::string &filePath);
+    
+    //draws source into certain part of the screen
+    void blitSurface(SDL_Texture * source, SDL_Rect * sourceRectangle, SDL_Rect * destinationRectangle);
+    
+    //renders everything to screen
+    void flip();
+    
+    //clears screen
+    void clear();
+    
+    SDL_Renderer * getRenderer() const;
+};
+
+#endif /* graphics_h */
